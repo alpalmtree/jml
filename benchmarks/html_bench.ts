8de4d24e476527985730f4458be2e8c45c4819a5
Al <alpalma95@gmail.com>
2025-05-25 10:43:41 +0200

    initial commit

---
+++ benchmarks/html_bench.ts
@@ -0,0 +1,70 @@
+import {  component, html, block } from "../html.ts";
+
+Deno.bench("html with components", { group: "rendering", baseline: true}, () => {
+ const Layout = component((h) => {
+  // deno-fmt-ignore
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        ${h.block("head")}
+      </head>
+      <body>
+        ${h.block("content")} 
+        ${h.block("scripts")}
+      </body>
+    </html>
+  `;
+});
+
+const Card = component<{ color: string }>((h, props) => {
+  return html`
+    <div class="card" style="border: 1px solid ${props?.color ?? "red"}">
+      ${h.children()}
+      <p>I'm inside the card component layout. Color: ${props?.color}</p>
+    </div>
+  `;
+});
+
+const P = component<{ text: string; color?: string }>((_, props) => {
+  return html`
+    <p style="color: ${props?.color ?? "black"}">
+      ${props?.text ??
+      "No text provided"}
+    </p>
+  `;
+});
+
+const page = () => {
+  // deno-fmt-ignore
+  return html`
+    ${Layout.open()} 
+
+        ${block('head', html`
+            <title>Hello from home</title>
+        `)}
+
+        ${block('content', html`
+              <h1>Home page</h1>
+              <p>Paragraph from home page</p>
+
+              ${P.render({ text: "Hi from void component", color: "red" })}
+
+              ${html`
+                  ${Card.open({ color: "green" })}
+                    <p>Hi there</p>
+                  ${Card.close()}
+              `} 
+          `)}
+
+        ${block('scripts', html`
+            <script>console.log("hi from home")</script>
+        `)}
+    ${Layout.close()}
+  `;
+};
+
+  page().string;
+});
---
+++ benchmarks/preact_bench.tsx
@@ -0,0 +1,63 @@
+import { render } from "preact-render-to-string";
+
+Deno.bench("preact jsx with components", { group: "rendering" }, () => {
+  // Naive implementation, as elements will be scaped
+  function Layout(
+    { head, children, scripts }: { head: any; children: any; scripts: any },
+  ) {
+    return (
+      <>
+        {"<!DOCTYPE html>"}
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            {head}
+          </head>
+          <body>
+            {children}
+            {scripts}
+          </body>
+        </html>
+      </>
+    );
+  }
+
+  function Card({ children, color }: { children: any; color?: string }) {
+    return (
+      <div class="card" style={`border: 1px solid ${color ?? "red"}`}>
+        {children}
+        I'm inside the card layout. Color: {color}
+      </div>
+    );
+  }
+
+  function P({ text, color }: { text?: string; color?: string }) {
+    return (
+      <p style={`color: ${color ?? "black"}`}>{text ?? "no text provided"}</p>
+    );
+  }
+
+  function Page() {
+    return (
+      <Layout
+        head={<title>Home Page</title>}
+        scripts={<script>console.log("hi from home")</script>}
+      >
+        <h1>Hi</h1>
+        <h1>Home page</h1>
+        <p>Paragraph from home page</p>
+        <P text="Hi from void component" color="green" />
+
+        <Card color="green">
+          <p>Hi there</p>
+        </Card>
+      </Layout>
+    );
+  }
+
+    render(<Page />);
+});
