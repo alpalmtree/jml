ecd9f31e5adbed91978a46b029e451adad7fdb44
Al <alpalma95@gmail.com>
2025-06-07 09:57:49 +0200

    rebuild

--- dist/html.d.ts
+++ dist/html.d.ts
@@ -24,7 +24,7 @@ declare module "src/html" {
         content: TemplateStringReturnValue;
     } & internalObject;
     class Macro<T = unknown> {
-        templateFunction: (helpers: Helpers, props?: T) => TemplateStringReturnValue;
+        templateFunction: (instance: Macro<T>) => TemplateStringReturnValue;
         props?: T;
         template: TemplateStringReturnValue;
         renderString: string;
@@ -35,18 +35,20 @@ declare module "src/html" {
         hasBlocks: boolean;
         hasChildren: boolean;
         blocks: Map<string, TemplateStringReturnValue>;
-        helpers: Helpers;
-        constructor(templateFunction: (helpers: Helpers, props?: T) => TemplateStringReturnValue, props?: T);
-        render(props?: T): RawString;
-        open(props?: T): MacroOpeningTag<T>;
+        h: Helpers;
+        constructor(templateFunction: (instance: Macro<T>) => TemplateStringReturnValue, props?: T);
+        render(): RawString;
+        open(): MacroOpeningTag<T>;
         close(): ClosingTag;
         html(strings: TemplateStringsArray, ...args: unknown[]): TemplateStringReturnValue;
-        with(props: T, callback: (macro: Macro<T>) => TemplateStringReturnValue): TemplateStringReturnValue;
     }
-    export const macro: <T>(cb: (helpers: Helpers, props?: T) => TemplateStringReturnValue) => Macro<T>;
+    export const macro: <T>(cb: (instance: Macro<T>) => TemplateStringReturnValue) => (props?: T) => Macro<T>;
     export const block: (name: string, content: TemplateStringReturnValue) => Block;
     export const raw: (value: string) => RawString;
     export const html: (strings: TemplateStringsArray, ...args: unknown[]) => TemplateStringReturnValue;
+    export const close: () => {
+        __type: string;
+    };
 }
 declare module "mod" {
     export * from "src/html";
